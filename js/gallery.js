d82d4428e93f9cf17f81a551383907e71c41aab4
SirWojtek <momatoku@gmail.com>
2015-06-08 19:30:41 +0200

    Added gallery plugin

---
+++ js/gallery.js
@@ -0,0 +1,44 @@
+var gallery = blueimp.Gallery([
+  {
+    href : 'https://www.youtube.com/watch?v=euTwYcp3eNM',
+    type : 'text/html',
+    youtube : 'euTwYcp3eNM',
+    poster : 'https://img.youtube.com/vi/euTwYcp3eNM/maxresdefault.jpg'
+  },
+  {
+    href : 'https://www.youtube.com/watch?v=xrHv1nj89Nk',
+    type : 'text/html',
+    youtube : 'xrHv1nj89Nk',
+    poster : 'https://img.youtube.com/vi/xrHv1nj89Nk/maxresdefault.jpg'
+  },
+  {
+    href : 'https://www.youtube.com/watch?v=OCSkXUNfhIQ',
+    type : 'text/html',
+    youtube : 'OCSkXUNfhIQ',
+    poster : 'https://img.youtube.com/vi/OCSkXUNfhIQ/maxresdefault.jpg'
+  },
+  {
+    href : 'https://www.youtube.com/watch?v=xhJmJXnfGzU',
+    type : 'text/html',
+    youtube : 'xhJmJXnfGzU',
+    poster : 'https://img.youtube.com/vi/xhJmJXnfGzU/maxresdefault.jpg'
+  },
+  {
+    href : 'https://www.youtube.com/watch?v=OwQdJ0LMXZo',
+    type : 'text/html',
+    youtube : 'OwQdJ0LMXZo',
+    poster : 'https://img.youtube.com/vi/OwQdJ0LMXZo/maxresdefault.jpg'
+  },
+  {
+    href : 'https://www.youtube.com/watch?v=S9ynstCMkcI',
+    type : 'text/html',
+    youtube : 'S9ynstCMkcI',
+    poster : 'https://img.youtube.com/vi/S9ynstCMkcI/maxresdefault.jpg'
+  },
+  {
+    href : 'https://www.youtube.com/watch?v=z6ArwvOKtCk',
+    type : 'text/html',
+    youtube : 'z6ArwvOKtCk',
+    poster : 'https://img.youtube.com/vi/z6ArwvOKtCk/maxresdefault.jpg'
+  }
+]);
